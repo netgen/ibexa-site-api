8dea1e3be42afa66e9368fa869f0abf8df1ac14a
Petar Španja <petar@spanja.info>
2022-12-14 14:39:23 +0100

    Add return type hint

--- bundle/View/Matcher/MatcherFactory.php
+++ bundle/View/Matcher/MatcherFactory.php
@@ -8,6 +8,7 @@ use Ibexa\Bundle\Core\Matcher\ViewMatcherRegistry;
 use Ibexa\Contracts\Core\Repository\Repository;
 use Ibexa\Contracts\Core\SiteAccess\ConfigResolverInterface;
 use Ibexa\Core\MVC\Symfony\Matcher\ClassNameMatcherFactory;
+use Ibexa\Core\MVC\Symfony\Matcher\ViewMatcherInterface;
 use Ibexa\Core\MVC\Symfony\View\View;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 use function mb_strpos;
@@ -56,7 +57,7 @@ class MatcherFactory extends ClassNameMatcherFactory
      *
      * @return \Ibexa\Core\MVC\Symfony\Matcher\ViewMatcherInterface
      */
-    protected function getMatcher($matcherIdentifier)
+    protected function getMatcher($matcherIdentifier): ViewMatcherInterface
     {
         if ($this->viewMatcherRegistry !== null && mb_strpos($matcherIdentifier, '@') === 0) {
             return $this->viewMatcherRegistry->getMatcher(mb_substr($matcherIdentifier, 1));
