556b548494d84344dd42d4ac8d90fde92d24ab31
Petar Španja <petar@spanja.info>
2023-02-07 16:45:15 +0100

    NGSTACK-714: override URL alias generator

---
+++ bundle/Routing/UrlAliasGenerator.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netgen\Bundle\IbexaSiteApiBundle\Routing;
+
+use Ibexa\Contracts\Core\Repository\Repository;
+use Ibexa\Contracts\Core\Repository\Values\Content\Location;
+use Ibexa\Contracts\Core\SiteAccess\ConfigResolverInterface;
+use Ibexa\Core\MVC\Symfony\Routing\Generator\UrlAliasGenerator as BaseUrlAliasGenerator;
+use Symfony\Component\Routing\RouterInterface;
+
+class UrlAliasGenerator extends BaseUrlAliasGenerator
+{
+    private Repository $repository;
+
+    public function __construct(
+        Repository $repository,
+        RouterInterface $defaultRouter,
+        ConfigResolverInterface $configResolver,
+        array $unsafeCharMap = []
+    ) {
+        parent::__construct($repository, $defaultRouter, $configResolver, $unsafeCharMap);
+
+        $this->repository = $repository;
+    }
+
+    public function loadLocation($locationId): Location
+    {
+        return $this->repository->sudo(
+            static function (Repository $repository) use ($locationId) {
+                return $repository->getLocationService()->loadLocation($locationId, []);
+            }
+        );
+    }
+}
