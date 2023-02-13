9881498c67f5fa2dacc49ff0a3ef9ae2289d26f4
Petar Španja <petar@spanja.info>
2023-02-13 09:13:42 +0100

    NGSTACK-714: cleanup exception context

--- bundle/Core/Site/LanguageResolver.php
+++ bundle/Core/Site/LanguageResolver.php
@@ -7,6 +7,7 @@ namespace Netgen\Bundle\IbexaSiteApiBundle\Core\Site;
 use Ibexa\Contracts\Core\Repository\Values\Content\Location;
 use Ibexa\Contracts\Core\Repository\Values\Content\VersionInfo;
 use Ibexa\Contracts\Core\SiteAccess\ConfigResolverInterface;
+use Ibexa\Core\MVC\Symfony\SiteAccess;
 use Netgen\Bundle\IbexaSiteApiBundle\SiteAccess\Resolver;
 use Netgen\IbexaSiteApi\API\LanguageResolver as BaseLanguageResolver;
 use Netgen\IbexaSiteApi\API\Settings as BaseSettings;
@@ -17,6 +18,7 @@ final class LanguageResolver extends BaseLanguageResolver
     private BaseSettings $settings;
     private Resolver $siteaccessResolver;
     private ConfigResolverInterface $configResolver;
+    private SiteAccess $currentSiteaccess;
 
     public function __construct(
         BaseSettings $settings,
@@ -28,6 +30,11 @@ final class LanguageResolver extends BaseLanguageResolver
         $this->configResolver = $configResolver;
     }
 
+    public function setSiteaccess(SiteAccess $currentSiteAccess = null): void
+    {
+        $this->currentSiteaccess = $currentSiteAccess;
+    }
+
     public function resolveByLanguage(VersionInfo $versionInfo, string $languageCode): string
     {
         if (in_array($languageCode, $versionInfo->languageCodes, true)) {
@@ -36,7 +43,16 @@ final class LanguageResolver extends BaseLanguageResolver
 
         throw new TranslationNotMatchedException(
             $versionInfo->contentInfo->id,
-            $this->getBaseContext($versionInfo, $languageCode)
+            [
+                'resolvedFor' => 'PREVIEW',
+                'currentSiteaccess' => $this->currentSiteaccess->name,
+                'content' => [
+                    'id' => $versionInfo->contentInfo->id,
+                    'translations' => $versionInfo->languageCodes,
+                    'versionNumber' => $versionInfo->versionNo,
+                ],
+                'givenLanguageCode' => $languageCode,
+            ],
         );
     }
 
@@ -55,16 +71,27 @@ final class LanguageResolver extends BaseLanguageResolver
             return $versionInfo->contentInfo->mainLanguageCode;
         }
 
-        $context = [
-            'resolvedSiteaccess' => $siteaccess,
-            'resolvedPrioritizedLanguages' => $prioritizedLanguages,
-            'resolvedUseAlwaysAvailable' => $this->getIsAlwaysAvailable($siteaccess),
-        ];
-        $context += $this->getBaseContext($versionInfo);
-
         throw new TranslationNotMatchedException(
             $versionInfo->contentInfo->id,
-            $context
+            [
+                'resolvedFor' => 'CONTENT',
+                'currentSiteaccess' => [
+                    'name' => $this->currentSiteaccess->name,
+                    'prioritizedLanguages' => $this->settings->prioritizedLanguages,
+                    'useAlwaysAvailable' => $this->settings->useAlwaysAvailable,
+                ],
+                'resolvedSiteaccess' => [
+                    'name' => $siteaccess,
+                    'prioritizedLanguages' => $prioritizedLanguages,
+                    'useAlwaysAvailable' => $this->getIsAlwaysAvailable($siteaccess),
+                ],
+                'content' => [
+                    'id' => $versionInfo->contentInfo->id,
+                    'translations' => $versionInfo->languageCodes,
+                    'mainTranslation' => $versionInfo->contentInfo->mainLanguageCode,
+                    'alwaysAvailable' => $versionInfo->contentInfo->alwaysAvailable,
+                ],
+            ],
         );
     }
 
@@ -86,17 +113,28 @@ final class LanguageResolver extends BaseLanguageResolver
             return $versionInfo->contentInfo->mainLanguageCode;
         }
 
-        $context = [
-            'locationId' => $location->id ?? null,
-            'resolvedSiteaccess' => $siteaccess,
-            'resolvedPrioritizedLanguages' => $prioritizedLanguages,
-            'resolvedUseAlwaysAvailable' => $this->getIsAlwaysAvailable($siteaccess),
-        ];
-        $context += $this->getBaseContext($versionInfo);
-
         throw new TranslationNotMatchedException(
             $versionInfo->contentInfo->id,
-            $context
+            [
+                'resolvedFor' => 'LOCATION',
+                'currentSiteaccess' => [
+                    'name' => $this->currentSiteaccess->name,
+                    'prioritizedLanguages' => $this->settings->prioritizedLanguages,
+                    'useAlwaysAvailable' => $this->settings->useAlwaysAvailable,
+                ],
+                'resolvedSiteaccess' => [
+                    'name' => $siteaccess,
+                    'prioritizedLanguages' => $prioritizedLanguages,
+                    'useAlwaysAvailable' => $this->getIsAlwaysAvailable($siteaccess),
+                ],
+                'content' => [
+                    'id' => $versionInfo->contentInfo->id,
+                    'locationId' => $location->id,
+                    'translations' => $versionInfo->languageCodes,
+                    'mainTranslation' => $versionInfo->contentInfo->mainLanguageCode,
+                    'alwaysAvailable' => $versionInfo->contentInfo->alwaysAvailable,
+                ],
+            ]
         );
     }
 
@@ -112,28 +150,5 @@ final class LanguageResolver extends BaseLanguageResolver
             null,
             $siteaccess
         );
-    }
-
-    /**
-     * Returns an array describing base language resolving context.
-     */
-    private function getBaseContext(VersionInfo $versionInfo, string $languageCode = null): array
-    {
-        $context = [
-            'currentPrioritizedLanguages' => $this->settings->prioritizedLanguages,
-            'currentUseAlwaysAvailable' => $this->settings->useAlwaysAvailable,
-            'availableTranslations' => $versionInfo->languageCodes,
-            'contentId' => $versionInfo->contentInfo->id,
-            'mainTranslation' => $versionInfo->contentInfo->mainLanguageCode,
-            'alwaysAvailable' => $versionInfo->contentInfo->alwaysAvailable,
-            'versionNumber' => $versionInfo->versionNo,
-            'isPublished' => $versionInfo->isPublished(),
-        ];
-
-        if ($languageCode !== null) {
-            $context['givenLanguageCode'] = $languageCode;
-        }
-
-        return $context;
     }
 }
